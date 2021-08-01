5805e6eebc91d2482a61f035ff0388209a8b29a5
Ori Shadmon <oshadmon@gmail.com>
2021-08-01 20:25:18 +0000

    in progress

--- deployment/main.py
+++ deployment/main.py
@@ -2,7 +2,16 @@ import argparse
 import os 
 
 def deployment(): 
+    """
+    Based on configuration file, deploy a specific node
+    :positional arguments:
+        config_file  AnyLog INI config file
+    """
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('config_file', type=str, default=None, help='AnyLog INI config file')
     args = parser.parse_args()
+    
+    config_file = os.path.expandvars(os.path.expanduser(config_file))
 
+if __name__ == '__main__': 
+    deployment()
