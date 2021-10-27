c3b244fa4479214a3b4820cf794ab5f92e9dd8b5
Oi Shadmon <oshadmon@gmail.com>
2021-10-27 10:38:23 -0700

    moved blockchain_cmd.blockchain_sync_scheduler to master/single_node

--- deployment/single_node.py
+++ deployment/single_node.py
@@ -1,5 +1,6 @@
 import __init__
 import anylog_api
+import blockchain_cmd
 import dbms_cmd
 import monitoring_cmd
 import policy_cmd
@@ -47,6 +48,9 @@ def single_node_init(conn:anylog_api.AnyLogConnect, config:dict, node_types:list
                 # Create ledger if not exists
                 if not dbms_cmd.create_table(conn=conn, db_name='blockchain', table_name='ledger', exception=exception):
                     print('Failed to create table blockchain.ledger')
+
+            blockchain_cmd.blockchain_sync_scheduler(conn=conn, source='dbms', time="30 seconds",
+                                                     master_node=config['master_node'], exception=exception)
 
             node_id = policy_cmd.declare_anylog_policy(conn=conn, policy_type=node, config=config,
                                                    master_node='local', disable_location=disable_location, exception=exception)
