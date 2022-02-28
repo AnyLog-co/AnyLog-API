643b9a52fa693c242aee98e616af17f6efa492a0
Oi Shadmon <oshadmon@gmail.com>
2022-02-27 17:46:59 -0800

    rename code

--- anylog_scripts/standalone.py
+++ anylog_scripts/standalone.py
@@ -2,8 +2,8 @@ import os
 import sys
 
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__)).split('anylog_scripts')[0]
-REST_PATH = os.path.join(ROOT_PATH, 'REST')
-sys.path.insert(0, REST_PATH)
+PYAPI_PATH = os.path.join(ROOT_PATH, 'pyapi')
+sys.path.insert(0, PYAPI_PATH)
 
 from anylog_connection import AnyLogConnection
 import authentication
