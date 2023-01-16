9244313d9a0d4a997d77f97302b4e1d00980fdb9
Ori Shadmon <oshadmon@gmail.com>
2023-01-16 12:14:33 -0800

    python packe-ing to test

--- python_rest/setup.py
+++ python_rest/setup.py
@@ -1,15 +1,6 @@
 # based on: https://towardsdatascience.com/how-to-upload-your-python-package-to-pypi-de1b363a1b3
 from setuptools import setup, find_packages
 
-def __get_requirements()->dict:
-    requirements = []
-    with open('requirements.txt', 'r') as f:
-        for line in f.readlines():
-            if '>=' in line:
-                requirements.append(line.split('>=')[0])
-    return requirements
-
-
 setup(
     name='anylog-network',
     version='1.0',
@@ -19,5 +10,10 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     keywords='anylog project',
-    install_requires=__get_requirements(),
+    install_requires=[
+        'ast',
+        'json',
+        'os',
+        'requests'
+    ],
 )
