59f3e237593444d3901200f9a6b68fce1eb23d3e
Ori Shadmon <oshadmon@gmail.com>
2023-01-16 12:17:09 -0800

    python packe-ing to test

--- python_rest/setup.py
+++ python_rest/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='anylog-network',
-    version='1.0',
+    version='0.1.0',
     license=None,
     author='AnyLog Co.',
     author_email='info@anylog.co',
