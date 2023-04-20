26a4d656bc7f5edeeda00ad83fd9c0154ab25047
Ori Shadmon <oshadmon@gmail.com>
2023-04-20 09:54:14 -0700

    execute_cmd and destination for post/get

--- python_rest/src/generic_get.py
+++ python_rest/src/generic_get.py
@@ -1,12 +1,15 @@
 import anylog_connector
 
 
-def get_cmd(anylog_conn:anylog_connector.AnyLogConnector, command:str, view_help:bool=False):
+def get_cmd(anylog_conn:anylog_connector.AnyLogConnector, command:str, destination:str=None, execute_cmd:bool=False,
+            view_help:bool=False):
     """
     execute a generic GET command
     :args:
         anylog_conn:anylog_connector.AnyLogConnector - Connection to AnyLog node
         cmd:str - command to execute
+        destination:str - destination to send request against (ie `run client`)
+        execute_cmd:bool - execute a given command, if False, then return command
         view_help:bool - execute `help` against `get status`
     :params:
         headers:dict - REST header information
@@ -21,11 +24,16 @@ def get_cmd(anylog_conn:anylog_connector.AnyLogConnector, command:str, view_help
     if view_help is True:
         anylog_connector.view_help(anylog_conn=anylog_conn, cmd=headers['command'])
         return None
+    elif execute_cmd is True:
+        if destination is not None:
+            headers["destination"] = destination
+        return anylog_conn.get(headers=headers)
 
-    return anylog_conn.get(headers=headers)
+    return headers["command"]
 
 
-def get_status(anylog_conn:anylog_connector.AnyLogConnector, json_format:bool=True, view_help:bool=False):
+def get_status(anylog_conn:anylog_connector.AnyLogConnector, json_format:bool=True, destination:str=None,
+               execute_cmd:bool=True, view_help:bool=False):
     """
     Check whether node is running or not
     :url:
@@ -33,43 +41,45 @@ def get_status(anylog_conn:anylog_connector.AnyLogConnector, json_format:bool=Tr
     :args:
         anylog_conn:anylog_connector.AnyLogConnector - Connection to AnyLog node
         json_format:bool - get result in JSON format
+        destination:str - destination to send request against (ie `run client`)
+        execute_cmd:bool - execute a given command, if False, then return command
         view_help:bool - execute `help` against `get status`
     :params:
         status:bool
         headers:dict - REST header information
         output - REST result
     :return:
-        None - help
-        False - failed
-        True - success
+        True/False if command gets executed (True if running false if Fails)
+        raw output if execute_cmd is False
     """
-    status = False
-    headers = {
-        "command": "get status",
-        "User-Agent": "AnyLog/1.23"
-    }
-
-    if view_help is True:
-        anylog_connector.view_help(anylog_conn=anylog_conn, cmd=headers['command'])
-        return None
+    command = "get status"
 
     if json_format is True:
-        headers["command"] += " where format=json"
+        command += " where format=json"
 
-    output = anylog_conn.get(headers=headers)
-    if output is not None and 'Status' in output and "running" in  output["Status"] and "not" not in output["Status"]:
-        status = True
+    output = get_cmd(anylog_conn=anylog_conn, command=command, destination=destination, execute_cmd=execute_cmd,
+                   view_help=view_help)
+    if execute_cmd is True:
+        status = False
+        if json_format is True and "Status" in output and "running" in output["Status"] and "not" not in output["Status"]:
+            status = True
+        elif "running" in output and "not" not in output:
+            status = True
+        return status
 
-    return status
+    return output
 
 
-def check_license(anylog_conn:anylog_connector.AnyLogConnector, view_help:bool=False):
+def check_license(anylog_conn:anylog_connector.AnyLogConnector, destination:str=None, execute_cmd:bool=True,
+                  view_help:bool=False):
     """
     Check whether license key is active or not
     :url:
         https://github.com/AnyLog-co/documentation/blob/master/anylog%20commands.md#get-command
     :args:
         anylog_conn:anylog_connector.AnyLogConnector - Connection to AnyLog node
+        destination:str - destination to send request against (ie `run client`)
+        execute_cmd:bool - execute a given command, if False, then return command
         view_help:bool - execute `help` against `get status`
     :params:
         status:bool
@@ -80,25 +90,14 @@ def check_license(anylog_conn:anylog_connector.AnyLogConnector, view_help:bool=F
         False - failed
         True - success
     """
-    status = True
-    headers = {
-        "command": "get license",
-        "User-Agent": "AnyLog/1.23"
-    }
+    command = "get license"
 
-    if view_help is True:
-        anylog_connector.view_help(anylog_conn=anylog_conn, cmd=headers['command'])
-        return None
-
-    output = anylog_conn.get(headers=headers)
-
-    if "Missing Valid AnyLog License Key" in output or "Activation license is missing" in output:
-        status = False
-
-    return status
+    return get_cmd(anylog_conn=anylog_conn, command=command, destination=destination, execute_cmd=execute_cmd,
+                   view_help=view_help)
 
 
-def get_dictionary(anylog_conn:anylog_connector.AnyLogConnector, json_format:bool=True, view_help:bool=False):
+def get_dictionary(anylog_conn:anylog_connector.AnyLogConnector, json_format:bool=True, destination:str=None,
+                   execute_cmd:bool=True, view_help:bool=False):
     """
     Extract dictionary from AnyLog
     :url:
@@ -106,6 +105,8 @@ def get_dictionary(anylog_conn:anylog_connector.AnyLogConnector, json_format:boo
     :args:
         anylog_conn:anylog_connector.AnyLogConnector - Connection to AnyLog node
         json_format:bool - get result in JSON format
+        destination:str - destination to send request against (ie `run client`)
+        execute_cmd:bool - execute a given command, if False, then return command
         view_help:bool - execute `help` against `get dictionary`
     :params:
         status:bool
@@ -114,22 +115,14 @@ def get_dictionary(anylog_conn:anylog_connector.AnyLogConnector, json_format:boo
         AnyLog dictionary
     """
     status = False
-    headers = {
-        "command": "get dictionary",
-        "User-Agent": "AnyLog/1.23"
-    }
+    command = "get dictionary"
 
-    if view_help is True:
-        anylog_connector.view_help(anylog_conn=anylog_conn, cmd=headers['command'])
-        return None
-
-    if json_format is True:
-        headers["command"] += " where format=json"
-
-    return anylog_conn.get(headers=headers)
+    return get_cmd(anylog_conn=anylog_conn, command=command, destination=destination, execute_cmd=execute_cmd,
+                   view_help=view_help)
 
 
-def get_processes(anylog_conn:anylog_connector.AnyLogConnector, json_format:bool=True, view_help:bool=False):
+def get_processes(anylog_conn:anylog_connector.AnyLogConnector, json_format:bool=True, destination:str=None,
+               execute_cmd:bool=True, view_help:bool=False):
     """
     View active processes in AnyLog
     :url:
@@ -137,6 +130,8 @@ def get_processes(anylog_conn:anylog_connector.AnyLogConnector, json_format:bool
     :args:
         anylog_conn:anylog_connector.AnyLogConnector - Connection to AnyLog node
         json_format:bool - get result in JSON format
+        destination:str - destination to send request against (ie `run client`)
+        execute_cmd:bool - execute a given command, if False, then return command
         view_help:bool - execute `help` against `get processes`
     :params:
         status:bool
@@ -144,23 +139,16 @@ def get_processes(anylog_conn:anylog_connector.AnyLogConnector, json_format:bool
     :return:
         AnyLog dictionary
     """
-    status = False
-    headers = {
-        "command": "get processes",
-        "User-Agent": "AnyLog/1.23"
-    }
-
-    if view_help is True:
-        anylog_connector.view_help(anylog_conn=anylog_conn, cmd=headers['command'])
-        return None
-
+    command = "get processes"
     if json_format is True:
-        headers["command"] += " where format=json"
+        command += " where format=json"
 
-    return anylog_conn.get(headers=headers)
+    return get_cmd(anylog_conn=anylog_conn, command=command, destination=destination, execute_cmd=execute_cmd,
+                   view_help=view_help)
 
 
-def get_scheduler(anylog_conn:anylog_connector.AnyLogConnector, schedule_number:int=None, view_help:bool=False):
+def get_scheduler(anylog_conn:anylog_connector.AnyLogConnector, schedule_number:int=None, destination:str=None,
+               execute_cmd:bool=True, view_help:bool=False):
     """
     Check if a scheduler is active or not
     :url:
@@ -168,6 +156,8 @@ def get_scheduler(anylog_conn:anylog_connector.AnyLogConnector, schedule_number:
     :args:
         anylog_conn:anylog_connector.AnyLogConnector - connection to AnyLog
         schedule_number:int - schedule number (ex `run schedule 1`
+        destination:str - destination to send request against (ie `run client`)
+        execute_cmd:bool - execute a given command, if False, then return command
         view_help:bool - execute `help` against `set license`
     :params:
         status:bool
@@ -179,32 +169,25 @@ def get_scheduler(anylog_conn:anylog_connector.AnyLogConnector, schedule_number:
             False if DNE
         else - return raw results of command
     """
-    status = True
-    headers = {
-        "command": "get scheduler",
-        "User-Agent": "AnyLog/1.23"
-    }
-
-    if view_help is True:
-        anylog_connector.view_help(anylog_conn=anylog_conn, cmd=headers['command'])
-        return None
-
+    command = "get scheduler"
     if schedule_number is not None:
-        headers["command"] += " " + str(schedule_number)
+        command += " " + str(schedule_number)
 
-    output = anylog_conn.get(headers=headers)
+    output = get_cmd(anylog_conn=anylog_conn, command=command, destination=destination, execute_cmd=execute_cmd,
+                     view_help=view_help)
 
-    if schedule_number is not None:
+    if execute_cmd is False or view_help is True: # print command or view help
         return output
-    elif schedule_number is not None and "not declared" in output:
-        status = False
-
-    print(status)
-    return status
+    elif execute_cmd is True and schedule_number is None: # print schedule
+        return output
+    elif "node declared" not in output and schedule_number is not None: # scheduled process is running
+        return True
+    else: # scheduled process(es) not running
+        return False
 
 
 def get_msg_client(anylog_conn:anylog_connector.AnyLogConnector, topic:str=None, broker:str=None, id:int=None,
-                   view_help:bool=False):
+                   destination: str = None, execute_cmd: bool = True, view_help:bool=False):
     """
     Check whether a msg client is active or not
     :url:
@@ -214,6 +197,8 @@ def get_msg_client(anylog_conn:anylog_connector.AnyLogConnector, topic:str=None,
         topic:str - topic to check for
         broker:str - IP:Port client is associated with
         id:int - client ID
+        destination:str - destination to send request against (ie `run client`)
+        execute_cmd:bool - execute a given command, if False, then return command
        view_help:bool - get info regarding command
     :params:
         headers:dict - REST header information
@@ -221,29 +206,23 @@ def get_msg_client(anylog_conn:anylog_connector.AnyLogConnector, topic:str=None,
     :return:
         results for command
     """
-    headers = {
-        "command": "get msg client",
-        "User-Agent": "AnyLog/1.23"
-    }
-
-    if view_help is True:
-        anylog_connector.view_help(anylog_conn=anylog_conn, cmd=headers['command'])
-        return None
+    command = "get msg client"
 
     if topic is not None or broker is not None or id is not None:
         count = 0
-        headers["command"] += " where"
+        command += " where"
         if topic is not None:
-            headers["command"] += f" topic={topic}"
+            command += f" topic={topic}"
             count += 1
         if broker is not None:
             if count >= 1:
-                headers["command"] += " and"
-            headers["command"] += f" broker={broker}"
+                command += " and"
+            command += f" broker={broker}"
             count += 1
         if id is not None:
             if count >= 1:
-                headers["command"] += " and"
-            headers["command"] += f" id={id}"
+                command += " and"
+            command += f" id={id}"
 
-    return anylog_conn.get(headers=headers)
+    return get_cmd(anylog_conn=anylog_conn, command=command, destination=destination, execute_cmd=execute_cmd,
+                   view_help=view_help)
