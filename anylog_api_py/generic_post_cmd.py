c1a8543f2ef3bcc0fd76c452088fe1d08d3156da
Ori Shadmon <oshadmon@gmail.com>
2023-09-06 13:26:35 -0700

    anylog configs

--- anylog_api_py/generic_post_cmd.py
+++ anylog_api_py/generic_post_cmd.py
@@ -28,6 +28,7 @@ def declare_configs(anylog_conn:AnyLogConnector, key:str, value:str, exception:b
         headers["command"] = f'set {key.strip()}="{value.strip()}"'
     else:
         headers["command"] = f"set {key.strip()}={value.strip()}"
+
     r, error = anylog_conn.post(headers=headers, payload=None)
     if r is False:
         status = False
@@ -35,4 +36,63 @@ def declare_configs(anylog_conn:AnyLogConnector, key:str, value:str, exception:b
             print_rest_error(call_type='POST', cmd=headers['command'], error=error)
 
     return status
+
+
+def declare_anylog_path(anylog_conn:AnyLogConnector, anylog_path:str, exception:bool=False)->bool:
+    """
+    Declare the location of the root directory to the AnyLog Files.
+    :url: 
+        https://github.com/AnyLog-co/documentation/blob/master/getting%20started.md#switching-between-different-setups
+    :args:
+        anylog_conn:AnyLogConnector - connection to AnyLog node
+        anylog_path:str - path to set AnyLog in
+        exception:bool - whether to print exceptions or not
+    :params:
+        status:bool
+        headers:dict - REST header
+    :return:
+        status
+    """
+    headers = {
+        "command": f"set anylog home {anylog_path}",
+        "User-Agent": "AnyLog/1.23"
+    }
+
+    r, error = anylog_conn.post(headers=headers, payload=None)
+    if r is False:
+        status = False
+        if exception is True:
+            print_rest_error(call_type='POST', cmd=headers['command'], error=error)
+
+    return status
+
+
+def create_work_directory(anylog_conn:AnyLogConnector, exception:bool=False)->bool:
+    """
+    Create the work directories at their default locations or locations configured using "set anylog home" command
+    :URL:
+        https://github.com/AnyLog-co/documentation/blob/master/getting%20started.md#local-directory-structure
+    :args:
+        anylog_conn:AnyLogConnector - connection to AnyLog node
+        exception:bool - whether to print exceptions or not
+    :params:
+        status:bool
+        headers:dict - REST headers
+    :return:
+        status
+    """
+    status = True
+    headers = {
+        "command": "create work directories",
+        "User-Agent": "AnyLog/1.23"
+    }
+
+    r, error = anylog_conn.post(headers=headers, payload=None)
+    if r is False:
+        status = False
+        if exception is True:
+            print_rest_error(call_type='POST', cmd=headers['command'], error=error)
+
+    return status
+
 
--- python_main/start_node.py
+++ python_main/start_node.py
@@ -9,6 +9,7 @@ import generic
 import anylog_api_py.support as support
 from anylog_api_py.anylog_connector import AnyLogConnector
 from anylog_api_py.generic_get_cmd import get_status
+from anylog_api_py.generic_post_cmd import declare_anylog_path, create_work_directory
 
 
 
@@ -18,13 +19,13 @@ ROOT_PATH = os.path.expanduser(os.path.expandvars('$HOME'))
 CONFIG_FILE = os.path.expanduser(os.path.expandvars(os.path.join(ROOT_PATH, 'deployments', 'docker-compose', 'anylog-generic', 'anylog_configs.env')))
 
 
-
 def main():
     """
     deployment script, but in (python) REST API format
     :process:
         1. extract connection information and validate connectivity
         2. set env configurations
+        3.set anylog_path and create work directories
     :positional arguments:
         conn:str                  REST credentials for the node to communicate with
         config_file               configuration file to utilize for deploying an AnyLog node
@@ -51,8 +52,17 @@ def main():
         print(f"Failed to connect to node {conn}, cannot continue...")
         exit(1)
 
+    # get dictionary information
     anylog_configs = generic.set_params(anylog_connector=anylog_connector, config_file=args.config_file, exception=args.exception)
-    print(anylog_configs)
+
+    if not declare_anylog_path(anylog_conn=anylog_connector, anylog_path=anylog_configs['anylog_path'], exception=args.exception):
+        print("Failed to declare Anylog path, cannot continue...")
+        exit(1)
+    if not create_work_directory(anylog_conn=anylog_connector, exception=args.exception):
+        print(f"Failed to create work directories within {anylog_configs['anylog_path']}")
+        exit(1)
+    print("Success!")
+
 
 
 
