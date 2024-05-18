554bb55fbd67411c0b3da40c2b74127e358c5df9
Ori Shadmon <oshadmon@gmail.com>
2024-05-18 15:50:51 -0700

    reorg / merge all
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

---
+++ anylog_api_main/generic_data_support.py
@@ -0,0 +1,106 @@
+def generate_run_mqtt_client(broker:str, port:int, username:str=None, password:str=None, topic:str='*',
+                             db_name:str=None, table_name:str=None, timestamp:str='timestamp', values:dict={},
+                             logs:bool=False, user_agent:bool=False)->str:
+    """
+    Generate `run mqtt client` command
+    :args:
+        broker:str - broker connection information
+        port:int port associated with broker
+        username:str - username credentials for broker
+        password:str - password for username
+        topic:str - specific topic to work against (if '*' then assumes all topic names)
+        db_nmae:str - logical database name
+        table_name:str - table name
+        timestamp:str - timestamp column
+        value:dict - dict of key/value pairs (ex. {'value': 'value'} || {'value': {'type': 'int', 'value': 'value'}}
+                     if no type is set, uses string (str)
+        logs:bool - whether to print MQTT logs
+        user_agent:bool - whether to use user-agent in `run mqtt`. if `broker` == rest sets to True by default
+    :params:
+        command:str - generated command
+    :return:
+        command
+    """
+    command = f'run mqtt client where broker={broker} and port={port}'
+    if broker == 'rest':
+        user_agent = True
+    if user_agent is True:
+        command += f' and user-agent=true'
+
+    if username is not None:
+        command += f' and user={username}'
+    if password is not None:
+        command += f' and password={password}'
+
+    command += f' and log=false'
+    if logs is True:
+        command += command.replace('log=false', 'log=true')
+
+    command += f' and topic=(name={topic}'
+    if db_name is not None:
+        command += f' and dbms={db_name}'
+    if table_name is not None:
+        command += f' and table={table_name}'
+    if timestamp is not None:
+        command += f' and column.timestamp.timestamp={timestamp}'
+
+    if values != {} and values is not None and isinstance(values, dict):
+        for column_name in values:
+            column_type = 'str'
+            value = None
+            if isinstance(values[column_name], dict):
+                if 'type' in values[column_name]:
+                    column_type = values[column_name]['type']
+                    if column_type not in ['int', 'float', 'bool', 'str']:
+                        column_type = 'str'
+                if 'value' in values[column_name]:
+                    value = values[column_name]['value']
+            else:
+                value = values[column_name]
+            if value is not None:
+                command += f' and column.{column_name}=(type={column_type} and value={value})'
+
+    command += ')'
+
+    return command
+
+
+def generate_query(db_name:str, query:str, format:str='json', stat:bool=True,
+                   include:str=None, extend:str=None, timezone:str='local')->str:
+    """
+    Generate SQL request
+    :args:
+        db_name:str - logical database to query against
+        query:str - query to execute (ex. select count(*) from rand_datq where timestamp >= NOW() - 1 day)
+        destination:str - whether to run query against the entire network or specific node(s). If set to None, then
+                          run without `destination`
+        format:str - result fromat (json || table)
+        stat:bool - whether to print statistics at the end of the query
+                    to return results in true JSON format - stat should be set to False
+        include:str  - comma separated "list" of tables to include in query
+        extend:str -   comma separated "list" of information to include in the results
+        timezone:str - results timezone (https://github.com/AnyLog-co/documentation/blob/master/queries.md#timezones)
+    :params:
+        command:str - generated SQL REST request
+    :return:
+        command
+    """
+    # build query
+    if format.split(':')[0].lower() not in ['json', 'table']:
+        format='json'
+    command = f'sql dbms={db_name} where format={format.lower()}'
+    if stat is True:
+        command += f' and stat=True'
+    else:
+        command += f' and stat=False'
+    if include is not None:
+        command += f' and include=({include})'
+    if extend is not None:
+        command += f' and extend={extend}'
+    if timezone is not None:
+        command += f' and timezone={timezone.lower()}'
+
+    command += f' {query}'
+
+    return command
+
---
+++ anylog_api_main/generic_get_calls.py
@@ -0,0 +1,337 @@
+# Generic GET:  https://github.com/AnyLog-co/documentation/blob/master/anylog%20commands.md#get-command
+
+from anylog_connector import AnyLogConnector
+import rest_support
+
+
+def get_status(anylog_conn:AnyLogConnector, json_format:bool=True, view_help:bool=False, exception:bool=False)->bool:
+    """
+    check if node is running
+    :url:
+        https://github.com/AnyLog-co/documentation/blob/master/monitoring%20nodes.md#the-get-status-command
+    :args:
+        anylog_conn:AnyLogConnector - AnyLog REST connection information
+        json_format:bool - whether to get the results in JSON format
+        view_:bool - whether to get help information for node
+        exception:bool - whether to print exceptions
+    :params:
+        status:bool
+        headers:dict - REST header information
+        r:bool, error:str - whether the command failed & why
+    :return:
+        True - running
+        False - not running
+        help - None
+    """
+    status = None
+    headers = {
+        'command': 'get status',
+        'User-Agent': 'AnyLog/1.23'
+    }
+
+    if json_format is True:
+        headers['command'] += f' where format=json'
+
+    if view_help is True:
+        help_command(anylog_conn=anylog_conn, command=headers['command'], exception=exception)
+    else:
+        r, error = anylog_conn.get(headers=headers)
+        if r is False:
+            status = False
+            if exception is True:
+                rest_support.print_rest_error(call_type='GET', cmd=headers['command'], error=error)
+        elif not isinstance(r, bool):
+            output = rest_support.extract_results(cmd=headers['command'], r=r, exception=exception)
+            if isinstance(output, dict):
+                output = output['status']
+            if 'running' in output and 'not' not in output:
+                status = True
+            else:
+                status = False
+
+    return status
+
+
+def get_dictionary(anylog_conn:AnyLogConnector, json_format:bool=True, view_help:bool=False, exception:bool=False)->str:
+    """
+    Extract AnyLog dictionary
+    :url:
+        https://github.com/AnyLog-co/documentation/blob/master/monitoring%20nodes.md#the-get-dictionary-command
+    :args:
+        anylog_conn:AnyLogConnector - AnyLog REST connection information
+        json_format:bool - whether to get results in JSON-dict format
+        view_help:bool - whether to view help for given command
+        exception:bool - whether to print exceptions
+    :params:
+        status:bool
+        headers:dict - REST header
+        anylog_dict:dict - results for dictionary
+        r:bool, error:str - whether the command failed & why
+    :return:
+        anylog_dict - empty dict if nothing gets returned or help is executed
+    """
+    anylog_dict = {}
+    headers = {
+        'command': 'get dictionary',
+        'User-Agent': 'AnyLog/1.23'
+    }
+    if json_format is True:
+        headers['command'] += ' where format=json'
+
+    if view_help is True:
+        help_command(anylog_conn=anylog_conn, command=headers['command'], exception=exception)
+    else:
+        r, error = anylog_conn.get(headers=headers)
+        if r is False and exception is True:
+            rest_support.print_rest_error(call_type='GET', cmd=headers['command'], error=error)
+        elif not isinstance(r, bool):
+            anylog_dict = rest_support.extract_results(cmd=headers['command'], r=r, exception=exception)
+
+    return anylog_dict
+
+
+def get_processes(anylog_conn:AnyLogConnector, json_format:bool=True, view_help:bool=False, exception:bool=False)->str:
+    """
+    view running / not declared processes
+    :url:
+        https://github.com/AnyLog-co/documentation/blob/master/monitoring%20nodes.md#the-get-processes-command
+    :args:
+        anylog_conn:AnyLogConnector - AnyLog REST connection information
+        json_format:bool - whether to get results in JSON format
+        view_help:bool - whether to get help info for command
+        exception:bool - whether to print exceptions
+    :params:
+        status:bool
+        processes_dict - dict of AnyLog processes
+        r:bool, error:str - whether the command failed & why
+    :return:
+        processes_dict
+    """
+    processes_dict = {}
+    headers = {
+        'command': 'get processes',
+        'User-Agent': 'AnyLog/1.23'
+    }
+
+    if json_format is True:
+        headers['command'] += ' where format=json'
+
+    if view_help is True:
+        help_command(anylog_conn=anylog_conn, command=headers['command'], exception=exception)
+    else:
+        r, error = anylog_conn.get(headers=headers)
+        if r is False and exception is True:
+            rest_support.print_rest_error(call_type='GET', cmd=headers['command'], error=error)
+        elif not isinstance(r, bool):
+            processes_dict = rest_support.extract_results(cmd=headers['command'], r=r, exception=exception)
+
+    return processes_dict
+
+
+def get_network_info(anylog_conn:AnyLogConnector, json_format:bool=True, view_help:bool=False, exception:bool=False)->str:
+    """
+    Get network information
+    :args:
+        anylog_conn:AnyLogConnector - AnyLog REST connection information
+        json_format:bool - whether to get results in JSON format
+        view_help:bool - whether to get help info for command
+        exception:bool - whether to print exceptions
+    :params:
+        status:bool
+        connections:dict - dict of AnyLog processes
+        r:bool, error:str - whether the command failed & why
+    :return:
+        connections
+    """
+    connections = {}
+    header={
+        'command': 'get connections',
+        'User-Agent': 'AnyLog/1.23'
+    }
+
+    if json_format is True:
+        header['command'] += ' where format=json'
+
+    if view_help is True:
+        help_command(anylog_conn=anylog_conn, command=header['command'], exception=exception)
+        connections = None
+    else:
+        r, error = anylog_conn.get(header=header)
+        if r is False:
+            if exception is True:
+                rest_support.print_rest_error(call_type='GET', cmd=header['command'], error=error)
+        elif not isinstance(r, bool):
+            connections = rest_support.extract_results(cmd=header['command'], r=r, exception=exception)
+
+    return connections
+
+
+def get_hostname(anylog_conn:AnyLogConnector, view_help:bool=False, exception:bool=False)->str:
+    """
+    Get hostname
+    :args:
+        anylog_conn:AnyLogConnector - AnyLog REST connection information
+        view_help:bool - print help information for command
+        exception:bool - whether to print exceptions
+    :params:
+        hostname:str - hostname
+        headers:dict - REST header information
+        r:bool, error:str - whether the command failed & why
+    :return:
+        hostname
+    """
+    hostname = None
+    headers = {
+        'command': 'get hostname',
+        'User-Agent': 'AnyLog/1.23'
+    }
+
+    if view_help is True:
+        help_command(anylog_conn=anylog_conn, command=headers['command'], exception=exception)
+    else:
+        r, error = anylog_conn.get(headers=headers)
+        if r is False and exception is True:
+            rest_support.print_rest_error(call_type='GET', cmd=headers['command'], error=error)
+        elif not isinstance(r, bool):
+            hostname = rest_support.extract_results(cmd=headers['command'], r=r, exception=exception)
+
+    return hostname
+
+
+def get_operator(anylog_conn:AnyLogConnector, view_help:bool=False, exception:bool=False)->str:
+    """
+    view data comming into operator node
+    :url:
+        https://github.com/AnyLog-co/documentation/blob/master/monitoring%20calls.md#get-operator
+    :args:
+        anylog_conn:AnyLogConnector - connect to AnyLog via REST
+        view_help:bool - whether to print help information
+        exception:bool - whether to print exceptions
+    :params:
+        output:str - output from GET request
+        headers:dict - REST header information
+        r:bool, error:str - whether the command failed & why
+    :return:
+        output
+    """
+    output = None
+    headers = {
+        'command': 'get operator',
+        'User-Agent': 'AnyLog/1.23'
+    }
+
+    if view_help is True:
+        help_command(anylog_conn=anylog_conn, command=headers['command'], exception=exception)
+    else:
+        r, error = anylog_conn.get(headers=headers)
+        if r is False and exception is True:
+            rest_support.print_rest_error(call_type='GET', cmd=headers['command'], error=error)
+        elif not isinstance(r, bool):
+            output = rest_support.extract_results(cmd=headers['command'], r=r, exception=exception)
+
+    return output
+
+
+def get_publisher(anylog_conn:AnyLogConnector, view_help:bool=False, exception:bool=False)->str:
+    """
+    view data coming into publisher node
+    :args:
+        anylog_conn:AnyLogConnector - connect to AnyLog via REST
+        view_help:bool - whether to print help information
+        exception:bool - whether to print exceptions
+    :params:
+        output:str - output from GET request
+        headers:dict - REST header information
+        r:bool, error:str - whether the command failed & why
+    :return:
+        output
+    """
+    output = None
+    headers = {
+        'command': 'get publisher',
+        'User-Agent': 'AnyLog/1.23'
+    }
+
+    if view_help is True:
+        help_command(anylog_conn=anylog_conn, command=headers['command'], exception=exception)
+    else:
+        r, error = anylog_conn.get(headers=headers)
+        if r is False and exception is True:
+            rest_support.print_rest_error(call_type='GET', cmd=headers['command'], error=error)
+        elif not isinstance(r, bool):
+            output = rest_support.extract_results(cmd=headers['command'], r=r, exception=exception)
+
+    return output
+
+
+def get_streaming(anylog_conn:AnyLogConnector, json_format:bool=True, view_help:bool=False, exception:bool=False)->str:
+    """
+    view streaming
+    :url:
+        https://github.com/AnyLog-co/documentation/blob/master/monitoring%20calls.md#get-streaming
+    :args:
+        anylog_conn:AnyLogConnector - connect to AnyLog via REST
+        json_Format:bool - get results in JSON format
+        view_help:bool - whether to print help information
+        exception:bool - whether to print exceptions
+    :params:
+        output:str - output from GET request
+        headers:dict - REST header information
+        r:bool, error:str - whether the command failed & why
+    :return:
+        output
+    """
+    output = None
+    headers = {
+        'command': 'get streaming',
+        'User-Agent': 'AnyLog/1.23'
+    }
+
+    if view_help is True:
+        help_command(anylog_conn=anylog_conn, command=headers['command'], exception=exception)
+    else:
+        if json_format is True:
+            headers['command'] += ' where format=json'
+
+        r, error = anylog_conn.get(headers=headers)
+        if r is False and exception is True:
+            rest_support.print_rest_error(call_type='GET', cmd=headers['command'], error=error)
+        elif not isinstance(r, bool):
+            output = rest_support.extract_results(cmd=headers['command'], r=r, exception=exception)
+
+    return output
+
+
+def help_command(anylog_conn:AnyLogConnector, command:str=None, exception:bool=False):
+    """
+    Given a command, get the `help` output for it
+    :url:
+        https://github.com/AnyLog-co/documentation/blob/master/getting%20started.md#the-help-command
+    :args:
+        anylog_connector:AnylogConnector - connection to AnyLog node
+        command:str - command to get help for
+        exception:bool - whether to print exceptions
+    :params:
+        output:str - help information
+        headers:dict - REST header information
+        r:requests.get, error - output for GET request
+    :print:
+        prints the results for `help` command
+    """
+    output = None
+    headers = {
+        'command': 'help',
+        'User-Agent': 'AnyLog/1.23'
+    }
+    if command is not None:
+        headers['command'] += f' {command}'
+
+    r, error = anylog_conn.get(headers=headers)
+    if r is False:
+        if exception is True:
+            rest_support.print_rest_error(call_type='GET', cmd=headers['command'], error=error)
+    elif not isinstance(r, bool):
+        output = rest_support.extract_results(cmd=headers['command'], r=r, exception=exception)
+
+    if output is not None:
+        print(output)
